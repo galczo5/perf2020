e83daaa89efa3f115c2ff6ee3c3324c98a0a213c
galczo5 <kamil.galek@gmail.com>
2020-10-09 19:26:15 +0200

    not styled table, plain

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import {Model} from '../data/model';
+import {MOCK_DATA} from '../data/MOCK_DATA';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'perf2020';
+  data: Array<Model> = [...MOCK_DATA, ...MOCK_DATA, ...MOCK_DATA, ...MOCK_DATA, ...MOCK_DATA];
 }
---
+++ src/data/model.ts
@@ -0,0 +1,14 @@
+export interface Model {
+  'id': number;
+  'first_name': string;
+  'last_name': string;
+  'email': string;
+  'gender': string;
+  'ip_address': string;
+  'city': string;
+  'company': string;
+  'country': string;
+  'type': string;
+  'title': string;
+  'color': string;
+}
